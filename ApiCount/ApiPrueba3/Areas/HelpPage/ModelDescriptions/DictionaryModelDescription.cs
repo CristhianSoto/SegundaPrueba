d55629fdf968d83fa636136f6e2308cb7616da71
CristhianSoto <a2018210980@estudiantes.upsa.edu.bo>
2021-03-19 18:59:06 -0400

    se completo la practica

---
+++ ApiCount/ApiPrueba3/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ApiPrueba3.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ApiCount/ApiPrueba3/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ApiPrueba3.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<ApiPrueba3.Models.Country> Countries { get; set; }
+    }
+}
